b26310ebebba9e12ea654af14e6404931151b82c
vimisharora <vimisharora@gmail.com>
2022-01-14 22:47:41 +0530

    blog migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :articles
+  get "articles/edit"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "articles#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
