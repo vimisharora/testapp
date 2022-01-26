780dc94eb39e0da22f1a1c4c4a9820dfb48938aa
vimisharora <vimisharora@gmail.com>
2022-01-26 20:01:04 +0530

    create comments completed- one to many associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles
-  get "articles/edit"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resources :comments
+  end
+  
   root "articles#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
